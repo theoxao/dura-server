b4a8bed8797f43141aff2973a4a3205ed30e3573
theo <theoxao@163.com>
2022-03-29 12:27:31 +0800

    update

--- recipe/src/main/kotlin/com/theoxao/duraemon/recipe/RecipeApplication.kt
+++ recipe/src/main/kotlin/com/theoxao/duraemon/recipe/RecipeApplication.kt
@@ -2,8 +2,10 @@ package com.theoxao.duraemon.recipe
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication(scanBasePackages = ["com.theoxao"])
+@EnableScheduling
 class RecipeApplication
 
 fun main(args: Array<String>) {
