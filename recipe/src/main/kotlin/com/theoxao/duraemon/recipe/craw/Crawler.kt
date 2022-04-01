6e4803315e83dfb8006e5a7a0ce46aa434f355ce
theo <theoxao@163.com>
2022-04-02 01:52:20 +0800

    update

--- recipe/src/main/kotlin/com/theoxao/duraemon/recipe/craw/Crawler.kt
+++ recipe/src/main/kotlin/com/theoxao/duraemon/recipe/craw/Crawler.kt
@@ -1,161 +1,161 @@
-package com.theoxao.duraemon.recipe.craw
-
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.theoxao.duraemon.orm.dto.Tables
-import com.theoxao.duraemon.orm.dto.Tables.TB_RECIPE
-import com.theoxao.duraemon.orm.dto.tables.records.ImageMapperRecord
-import com.theoxao.duraemon.orm.dto.tables.records.TbRecipeRecord
-import com.theoxao.duraemon.recipe.model.RecipeModel
-import com.theoxao.duraemon.recipe.model.ResponseWrapper
-import okhttp3.*
-import okhttp3.internal.closeQuietly
-import org.jooq.DSLContext
-import org.jooq.JSON
-import org.jooq.impl.DSL
-import org.slf4j.LoggerFactory
-import org.springframework.scheduling.annotation.Scheduled
-import org.springframework.stereotype.Component
-import java.io.IOException
-import java.time.Duration
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.util.*
-import javax.annotation.Resource
-
-val datetimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-
-@Component
-class Crawler {
-
-    private val log = LoggerFactory.getLogger(this::class.java)
-
-    @Resource(name = "masterDSLContext")
-    lateinit var masterDSLContext: DSLContext
-
-
-    @Resource(name="slaveDSLContext")
-    lateinit var slaveDSLContent: DSLContext
-
-    @Resource
-    lateinit var objectMapper: ObjectMapper
-
-    val http = OkHttpClient.Builder()
-        .callTimeout(Duration.ofSeconds(30))
-        .readTimeout(Duration.ofSeconds(30))
-        .writeTimeout(Duration.ofSeconds(30))
-        .connectTimeout(Duration.ofSeconds(30)).build()
-
-//    @PostConstruct
-    fun initSchedule(){
-        scheduled()
-    }
-
-
-    @Scheduled(cron = "* * 12 1/1 * ?")
-    fun scheduled() {
-        val max = masterDSLContext.select(DSL.max(TB_RECIPE.ID)).from(TB_RECIPE).fetchAny()?.value1()!!
-        ((max-2000)..(max+2000)).toMutableList().craw()
-    }
-
-    fun RecipeModel?.toRecipe():TbRecipeRecord? {
-        return this?.let { recipe ->
-            TB_RECIPE.newRecord().apply {
-                this.id = recipe.id
-                this.desc = recipe.desc
-                this.difficulty = recipe.difficulty.toJson()
-                this.image = recipe.image.toJson()
-                this.name = recipe.name
-                this.summary = recipe.summary
-                this.tips = recipe.tips
-                this.label = recipe.labels.toJson()
-                this.createTime = recipe.createTime?.let { LocalDateTime.parse(it, datetimeFormatter) }
-                this.ingredient = recipe.ingredient.toJson()
-                this.instruction = recipe.instruction.toJson()
-                this.duration = recipe.duration.toJson()
-                this.authorName = recipe.author?.name
-                this.authorId = recipe.author?.id
-                this.pv = recipe.stats?.nPv
-                this.collect = recipe.stats?.nCollects
-                this.comment = recipe.stats?.nComments
-                this.cooked = recipe.stats?.nCooked
-            }
-        }
-    }
-
-    fun Any?.toJson(): JSON?=  this?.let { JSON.valueOf(objectMapper.writeValueAsString(it)) }
-
-    fun MutableList<Int>.craw() {
-            this.forEach { id ->
-                val request = Request.Builder()
-                    .url("https://www.xiachufang.com/juno/api/v2/recipes/show_v2.json?id=${id}&mode=full")
-                    .get()
-                    .addHeader(
-                        "User-Agent",
-                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36"
-                    )
-                    .build()
-                http.newCall(request).enqueue(object : Callback {
-                    override fun onFailure(call: Call, e: IOException) {
-                        log.error(e.message, e)
-                    }
-                    override fun onResponse(call: Call, response: Response) {
-                        if (LocalDateTime.now().second == 0) {
-                            log.info(">>>>>queued call count:{}", http.dispatcher.queuedCallsCount())
-                        }
-                        if (response.isSuccessful) {
-                            response.body?.string()?.let { json ->
-                                val resp = objectMapper.readValue(json, ResponseWrapper::class.java)
-                                if ( resp.status != "ok") return@let
-                                resp.content?.get("recipe")?.let { recipe->
-                                    val mapperList = arrayListOf<ImageMapperRecord>()
-                                    recipe.image?.ident?.let { ident->
-                                        mapperList.add(Tables.IMAGE_MAPPER.newRecord().apply {
-                                            val id = UUID.randomUUID().toString()
-                                            this.uuid= id
-                                            recipe.image?.uuid =   id
-                                            this.oid = recipe.id
-                                            this.type = 0
-                                            this.imageUrl = ident
-                                        })
-                                    }
-                                    recipe.instruction?.filterNotNull()?.forEach {
-                                        it.image?.ident?.let { ident->
-                                            mapperList.add(Tables.IMAGE_MAPPER.newRecord().apply {
-                                                val uid = UUID.randomUUID().toString()
-                                                this.uuid= uid
-                                                it.image?.uuid = uid
-                                                this.oid = recipe.id
-                                                this.type = 0
-                                                this.imageUrl = ident
-                                            })
-                                        }
-                                        it.video?.url?.let { url->
-                                            mapperList.add(Tables.IMAGE_MAPPER.newRecord().apply {
-                                                val uid = UUID.randomUUID().toString()
-                                                this.uuid= uid
-                                                it.video?.uuid = uid
-                                                this.oid = recipe.id
-                                                this.type = 1
-                                                this.imageUrl = url
-                                            })
-                                        }
-                                    }
-                                    val record = recipe.toRecipe()
-                                    masterDSLContext.transaction { config ->
-                                        record?.let {
-                                            DSL.using(config).insertInto(TB_RECIPE).set(it).onConflictDoNothing().execute()
-                                        }
-                                        if (mapperList.isNotEmpty()){
-                                            DSL.using(config).batchInsert(mapperList).execute()
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        log.info("request@{}, response code:{} ", id, response.code)
-                        response.closeQuietly()
-                    }
-                })
-        }
-    }
-}
+//package com.theoxao.duraemon.recipe.craw
+//
+//import com.fasterxml.jackson.databind.ObjectMapper
+//import com.theoxao.duraemon.orm.dto.Tables
+//import com.theoxao.duraemon.orm.dto.Tables.TB_RECIPE
+//import com.theoxao.duraemon.orm.dto.tables.records.ImageMapperRecord
+//import com.theoxao.duraemon.orm.dto.tables.records.TbRecipeRecord
+//import com.theoxao.duraemon.recipe.model.RecipeModel
+//import com.theoxao.duraemon.recipe.model.ResponseWrapper
+//import okhttp3.*
+//import okhttp3.internal.closeQuietly
+//import org.jooq.DSLContext
+//import org.jooq.JSON
+//import org.jooq.impl.DSL
+//import org.slf4j.LoggerFactory
+//import org.springframework.scheduling.annotation.Scheduled
+//import org.springframework.stereotype.Component
+//import java.io.IOException
+//import java.time.Duration
+//import java.time.LocalDateTime
+//import java.time.format.DateTimeFormatter
+//import java.util.*
+//import javax.annotation.Resource
+//
+//val datetimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+//
+//@Component
+//class Crawler {
+//
+//    private val log = LoggerFactory.getLogger(this::class.java)
+//
+//    @Resource(name = "masterDSLContext")
+//    lateinit var masterDSLContext: DSLContext
+//
+//
+//    @Resource(name="slaveDSLContext")
+//    lateinit var slaveDSLContent: DSLContext
+//
+//    @Resource
+//    lateinit var objectMapper: ObjectMapper
+//
+//    val http = OkHttpClient.Builder()
+//        .callTimeout(Duration.ofSeconds(30))
+//        .readTimeout(Duration.ofSeconds(30))
+//        .writeTimeout(Duration.ofSeconds(30))
+//        .connectTimeout(Duration.ofSeconds(30)).build()
+//
+////    @PostConstruct
+//    fun initSchedule(){
+//        scheduled()
+//    }
+//
+//
+//    @Scheduled(cron = "* * 12 1/1 * ?")
+//    fun scheduled() {
+//        val max = masterDSLContext.select(DSL.max(TB_RECIPE.ID)).from(TB_RECIPE).fetchAny()?.value1()!!
+//        ((max-2000)..(max+2000)).toMutableList().craw()
+//    }
+//
+//    fun RecipeModel?.toRecipe():TbRecipeRecord? {
+//        return this?.let { recipe ->
+//            TB_RECIPE.newRecord().apply {
+//                this.id = recipe.id
+//                this.desc = recipe.desc
+//                this.difficulty = recipe.difficulty.toJson()
+//                this.image = recipe.image.toJson()
+//                this.name = recipe.name
+//                this.summary = recipe.summary
+//                this.tips = recipe.tips
+//                this.label = recipe.labels.toJson()
+//                this.createTime = recipe.createTime?.let { LocalDateTime.parse(it, datetimeFormatter) }
+//                this.ingredient = recipe.ingredient.toJson()
+//                this.instruction = recipe.instruction.toJson()
+//                this.duration = recipe.duration.toJson()
+//                this.authorName = recipe.author?.name
+//                this.authorId = recipe.author?.id
+//                this.pv = recipe.stats?.nPv
+//                this.collect = recipe.stats?.nCollects
+//                this.comment = recipe.stats?.nComments
+//                this.cooked = recipe.stats?.nCooked
+//            }
+//        }
+//    }
+//
+//    fun Any?.toJson(): JSON?=  this?.let { JSON.valueOf(objectMapper.writeValueAsString(it)) }
+//
+//    fun MutableList<Int>.craw() {
+//            this.forEach { id ->
+//                val request = Request.Builder()
+//                    .url("https://www.xiachufang.com/juno/api/v2/recipes/show_v2.json?id=${id}&mode=full")
+//                    .get()
+//                    .addHeader(
+//                        "User-Agent",
+//                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36"
+//                    )
+//                    .build()
+//                http.newCall(request).enqueue(object : Callback {
+//                    override fun onFailure(call: Call, e: IOException) {
+//                        log.error(e.message, e)
+//                    }
+//                    override fun onResponse(call: Call, response: Response) {
+//                        if (LocalDateTime.now().second == 0) {
+//                            log.info(">>>>>queued call count:{}", http.dispatcher.queuedCallsCount())
+//                        }
+//                        if (response.isSuccessful) {
+//                            response.body?.string()?.let { json ->
+//                                val resp = objectMapper.readValue(json, ResponseWrapper::class.java)
+//                                if ( resp.status != "ok") return@let
+//                                resp.content?.get("recipe")?.let { recipe->
+//                                    val mapperList = arrayListOf<ImageMapperRecord>()
+//                                    recipe.image?.ident?.let { ident->
+//                                        mapperList.add(Tables.IMAGE_MAPPER.newRecord().apply {
+//                                            val id = UUID.randomUUID().toString()
+//                                            this.uuid= id
+//                                            recipe.image?.uuid =   id
+//                                            this.oid = recipe.id
+//                                            this.type = 0
+//                                            this.imageUrl = ident
+//                                        })
+//                                    }
+//                                    recipe.instruction?.filterNotNull()?.forEach {
+//                                        it.image?.ident?.let { ident->
+//                                            mapperList.add(Tables.IMAGE_MAPPER.newRecord().apply {
+//                                                val uid = UUID.randomUUID().toString()
+//                                                this.uuid= uid
+//                                                it.image?.uuid = uid
+//                                                this.oid = recipe.id
+//                                                this.type = 0
+//                                                this.imageUrl = ident
+//                                            })
+//                                        }
+//                                        it.video?.url?.let { url->
+//                                            mapperList.add(Tables.IMAGE_MAPPER.newRecord().apply {
+//                                                val uid = UUID.randomUUID().toString()
+//                                                this.uuid= uid
+//                                                it.video?.uuid = uid
+//                                                this.oid = recipe.id
+//                                                this.type = 1
+//                                                this.imageUrl = url
+//                                            })
+//                                        }
+//                                    }
+//                                    val record = recipe.toRecipe()
+//                                    masterDSLContext.transaction { config ->
+//                                        record?.let {
+//                                            DSL.using(config).insertInto(TB_RECIPE).set(it).onConflictDoNothing().execute()
+//                                        }
+//                                        if (mapperList.isNotEmpty()){
+//                                            DSL.using(config).batchInsert(mapperList).execute()
+//                                        }
+//                                    }
+//                                }
+//                            }
+//                        }
+//                        log.info("request@{}, response code:{} ", id, response.code)
+//                        response.closeQuietly()
+//                    }
+//                })
+//        }
+//    }
+//}
--- recipe/src/main/kotlin/com/theoxao/duraemon/recipe/craw/Job.kt
+++ recipe/src/main/kotlin/com/theoxao/duraemon/recipe/craw/Job.kt
@@ -1,100 +1,100 @@
-package com.theoxao.duraemon.recipe.craw
-
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.theoxao.duraemon.orm.dto.Tables.*
-import com.theoxao.duraemon.orm.dto.tables.records.ImageMapperRecord
-import com.theoxao.duraemon.recipe.model.ResponseWrapper
-import okhttp3.OkHttpClient
-import org.jooq.DSLContext
-import org.jooq.JSON
-import org.jooq.impl.DSL
-import org.slf4j.LoggerFactory
-import org.springframework.stereotype.Component
-import java.time.Duration
-import java.util.*
-import javax.annotation.PostConstruct
-import javax.annotation.Resource
-
-@Component
-class Job {
-
-    private val log = LoggerFactory.getLogger(this::class.java)
-
-    @Resource(name = "masterDSLContext")
-    lateinit var masterDSLContext: DSLContext
-
-
-    @Resource(name="slaveDSLContext")
-    lateinit var slaveDSLContent: DSLContext
-
-    @Resource
-    lateinit var objectMapper: ObjectMapper
-
-    val http = OkHttpClient.Builder()
-        .callTimeout(Duration.ofSeconds(30))
-        .readTimeout(Duration.ofSeconds(30))
-        .writeTimeout(Duration.ofSeconds(30))
-        .connectTimeout(Duration.ofSeconds(30)).build()
-
-    fun Any?.toJson(): JSON?=  this?.let { JSON.valueOf(objectMapper.writeValueAsString(it)) }
-
-//    @PostConstruct
-    fun init(){
-        val batch = 1000
-        var last:Int? = 0
-        while (true){
-            val list =slaveDSLContent.selectFrom(RECIPE_JSON)
-                .where(RECIPE_JSON.ID.gt(last))
-                .orderBy(RECIPE_JSON.ID).limit(batch).fetch()
-            last = list.lastOrNull()?.id
-            if (list.isEmpty() || last == null) break
-            list.forEach { record->
-                log.info("id@{}", record.id)
-                val wrapper = objectMapper.readValue(record.recipeJson.data(), ResponseWrapper::class.java)
-                wrapper.content?.get("recipe")?.let { recipe->
-                    val mapperList = arrayListOf<ImageMapperRecord>()
-                    recipe.image?.ident?.let { ident->
-                        mapperList.add(IMAGE_MAPPER.newRecord().apply {
-                            val id = UUID.randomUUID().toString()
-                            this.uuid= id
-                            recipe.image?.uuid =   id
-                            this.oid = record.id
-                            this.type = 0
-                            this.imageUrl = ident
-                        })
-                    }
-                    recipe.instruction?.filterNotNull()?.forEach {
-                        it.image?.ident?.let { ident->
-                            mapperList.add(IMAGE_MAPPER.newRecord().apply {
-                                val id = UUID.randomUUID().toString()
-                                this.uuid= id
-                                it.image?.uuid = id
-                                this.oid = record.id
-                                this.type = 0
-                                this.imageUrl = ident
-                            })
-                        }
-                        it.video?.url?.let { url->
-                            mapperList.add(IMAGE_MAPPER.newRecord().apply {
-                                val id = UUID.randomUUID().toString()
-                                this.uuid= id
-                                it.video?.uuid = id
-                                this.oid = record.id
-                                this.type = 1
-                                this.imageUrl = url
-                            })
-                        }
-                    }
-                    masterDSLContext.transaction { config->
-                        if (mapperList.isNotEmpty())
-                            DSL.using(config).batchInsert(mapperList).execute()
-                        DSL.using(config).update(TB_RECIPE)
-                            .set(TB_RECIPE.INGREDIENT, recipe.ingredient.toJson())
-                            .set(TB_RECIPE.INSTRUCTION , recipe.instruction.toJson())
-                            .where(TB_RECIPE.ID.eq(recipe.id)).execute()
-                    }
-                }
-            }
-        }
-    }
-}
+//package com.theoxao.duraemon.recipe.craw
+//
+//import com.fasterxml.jackson.databind.ObjectMapper
+//import com.theoxao.duraemon.orm.dto.Tables.*
+//import com.theoxao.duraemon.orm.dto.tables.records.ImageMapperRecord
+//import com.theoxao.duraemon.recipe.model.ResponseWrapper
+//import okhttp3.OkHttpClient
+//import org.jooq.DSLContext
+//import org.jooq.JSON
+//import org.jooq.impl.DSL
+//import org.slf4j.LoggerFactory
+//import org.springframework.stereotype.Component
+//import java.time.Duration
+//import java.util.*
+//import javax.annotation.PostConstruct
+//import javax.annotation.Resource
+//
+//@Component
+//class Job {
+//
+//    private val log = LoggerFactory.getLogger(this::class.java)
+//
+//    @Resource(name = "masterDSLContext")
+//    lateinit var masterDSLContext: DSLContext
+//
+//
+//    @Resource(name="slaveDSLContext")
+//    lateinit var slaveDSLContent: DSLContext
+//
+//    @Resource
+//    lateinit var objectMapper: ObjectMapper
+//
+//    val http = OkHttpClient.Builder()
+//        .callTimeout(Duration.ofSeconds(30))
+//        .readTimeout(Duration.ofSeconds(30))
+//        .writeTimeout(Duration.ofSeconds(30))
+//        .connectTimeout(Duration.ofSeconds(30)).build()
+//
+//    fun Any?.toJson(): JSON?=  this?.let { JSON.valueOf(objectMapper.writeValueAsString(it)) }
+//
+////    @PostConstruct
+//    fun init(){
+//        val batch = 1000
+//        var last:Int? = 0
+//        while (true){
+//            val list =slaveDSLContent.selectFrom(RECIPE_JSON)
+//                .where(RECIPE_JSON.ID.gt(last))
+//                .orderBy(RECIPE_JSON.ID).limit(batch).fetch()
+//            last = list.lastOrNull()?.id
+//            if (list.isEmpty() || last == null) break
+//            list.forEach { record->
+//                log.info("id@{}", record.id)
+//                val wrapper = objectMapper.readValue(record.recipeJson.data(), ResponseWrapper::class.java)
+//                wrapper.content?.get("recipe")?.let { recipe->
+//                    val mapperList = arrayListOf<ImageMapperRecord>()
+//                    recipe.image?.ident?.let { ident->
+//                        mapperList.add(IMAGE_MAPPER.newRecord().apply {
+//                            val id = UUID.randomUUID().toString()
+//                            this.uuid= id
+//                            recipe.image?.uuid =   id
+//                            this.oid = record.id
+//                            this.type = 0
+//                            this.imageUrl = ident
+//                        })
+//                    }
+//                    recipe.instruction?.filterNotNull()?.forEach {
+//                        it.image?.ident?.let { ident->
+//                            mapperList.add(IMAGE_MAPPER.newRecord().apply {
+//                                val id = UUID.randomUUID().toString()
+//                                this.uuid= id
+//                                it.image?.uuid = id
+//                                this.oid = record.id
+//                                this.type = 0
+//                                this.imageUrl = ident
+//                            })
+//                        }
+//                        it.video?.url?.let { url->
+//                            mapperList.add(IMAGE_MAPPER.newRecord().apply {
+//                                val id = UUID.randomUUID().toString()
+//                                this.uuid= id
+//                                it.video?.uuid = id
+//                                this.oid = record.id
+//                                this.type = 1
+//                                this.imageUrl = url
+//                            })
+//                        }
+//                    }
+//                    masterDSLContext.transaction { config->
+//                        if (mapperList.isNotEmpty())
+//                            DSL.using(config).batchInsert(mapperList).execute()
+//                        DSL.using(config).update(TB_RECIPE)
+//                            .set(TB_RECIPE.INGREDIENT, recipe.ingredient.toJson())
+//                            .set(TB_RECIPE.INSTRUCTION , recipe.instruction.toJson())
+//                            .where(TB_RECIPE.ID.eq(recipe.id)).execute()
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
