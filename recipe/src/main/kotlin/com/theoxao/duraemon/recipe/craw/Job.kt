dd975efadbdbfb35b72b1f7a9b6b64d109aa809e
theo <theoxao@163.com>
2022-04-03 18:06:56 +0800

    update

--- recipe/src/main/kotlin/com/theoxao/duraemon/recipe/craw/Job.kt
+++ recipe/src/main/kotlin/com/theoxao/duraemon/recipe/craw/Job.kt
@@ -1,8 +1,10 @@
 package com.theoxao.duraemon.recipe.craw
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.theoxao.duraemon.orm.dto.Tables.*
+import com.theoxao.duraemon.orm.dto.Tables.IMAGE_MAPPER
+import com.theoxao.duraemon.orm.dto.Tables.TB_RECIPE
 import com.theoxao.duraemon.orm.dto.tables.records.ImageMapperRecord
+import com.theoxao.duraemon.recipe.model.RecipeModel
 import com.theoxao.duraemon.recipe.model.ResponseWrapper
 import okhttp3.*
 import okhttp3.internal.closeQuietly
@@ -35,20 +37,20 @@ class Job {
         .writeTimeout(Duration.ofSeconds(30))
         .connectTimeout(Duration.ofSeconds(30)).build()
 
-    fun Any?.toJson(): JSON?=  this?.let { JSON.valueOf(objectMapper.writeValueAsString(it)) }
+    fun Any?.toJson(): JSON? = this?.let { JSON.valueOf(objectMapper.writeValueAsString(it)) }
 
     @PostConstruct
-    fun init(){
+    fun init() {
         val batch = 1000
-        var last:Int? = 0
-        while (true){
-            val list =masterDSLContext.selectFrom(TB_RECIPE)
+        var last: Int? = 0
+        while (true) {
+            val list = masterDSLContext.selectFrom(TB_RECIPE)
                 .where(TB_RECIPE.ID.lt(last))
                 .and(TB_RECIPE.UPDATE_TIME.le(LocalDateTime.now().minusDays(1)))
                 .orderBy(TB_RECIPE.ID.desc()).limit(batch).fetch()
             last = list.lastOrNull()?.id
             if (list.isEmpty() || last == null) break
-            list.forEach { record->
+            list.forEach { record ->
                 val request = Request.Builder()
                     .url("https://www.xiachufang.com/juno/api/v2/recipes/show_v2.json?id=${record.id}&mode=full")
                     .get()
@@ -61,41 +63,46 @@ class Job {
                     override fun onFailure(call: Call, e: IOException) {
                         log.error(e.message, e)
                     }
+
                     override fun onResponse(call: Call, response: Response) {
-                        if (response.isSuccessful){
+                        if (response.isSuccessful) {
                             val wrapper = objectMapper.readValue(response.body?.string(), ResponseWrapper::class.java)
                             if (wrapper.status != "ok") {
-                                log.info("id@{} is not ok", record.id)
-                                return
-                            }
-                            log.info("id@{} is ok", record.id)
-                            wrapper.content?.get("recipe")?.let { recipe->
                                 val mapperList = arrayListOf<ImageMapperRecord>()
-                                recipe.image?.ident?.let { ident->
+                                val image = objectMapper.readValue(record.image.data(), RecipeModel.Image::class.java)
+                                image?.ident?.let { ident ->
                                     mapperList.add(IMAGE_MAPPER.newRecord().apply {
-                                        val id = UUID.randomUUID().toString()
-                                        this.uuid= id
-                                        recipe.image?.uuid =   id
+                                        val uid = UUID.randomUUID().toString()
+                                        this.uuid = uid
+                                        image.uuid = uid
+                                        record.image = image.toJson()
                                         this.oid = record.id
                                         this.type = 0
                                         this.imageUrl = ident
                                     })
                                 }
-                                recipe.instruction?.filterNotNull()?.forEach {
-                                    it.image?.ident?.let { ident->
+                                val instrs = objectMapper.readValue<List<RecipeModel.Instruction>>(
+                                    record.instruction.data(),
+                                    objectMapper.typeFactory.constructParametricType(
+                                        List::class.java,
+                                        RecipeModel.Instruction::class.java
+                                    )
+                                )
+                                instrs?.forEach {
+                                    it.image?.ident?.let { ident ->
                                         mapperList.add(IMAGE_MAPPER.newRecord().apply {
                                             val id = UUID.randomUUID().toString()
-                                            this.uuid= id
+                                            this.uuid = id
                                             it.image?.uuid = id
                                             this.oid = record.id
                                             this.type = 0
                                             this.imageUrl = ident
                                         })
                                     }
-                                    it.video?.url?.let { url->
+                                    it.video?.url?.let { url ->
                                         mapperList.add(IMAGE_MAPPER.newRecord().apply {
                                             val id = UUID.randomUUID().toString()
-                                            this.uuid= id
+                                            this.uuid = id
                                             it.video?.uuid = id
                                             this.oid = record.id
                                             this.type = 1
@@ -103,12 +110,57 @@ class Job {
                                         })
                                     }
                                 }
-                                masterDSLContext.transaction { config->
+                                record.instruction = instrs.toJson()
+                                masterDSLContext.transaction { config ->
+                                    if (mapperList.isNotEmpty())
+                                        DSL.using(config).batchInsert(mapperList).execute()
+                                    DSL.using(config).executeUpdate(record)
+                                }
+                                log.info("id@{} is not ok", record.id)
+                                response.closeQuietly()
+                                return
+                            }
+                            log.info("id@{} is ok", record.id)
+                            wrapper.content?.get("recipe")?.let { recipe ->
+                                val mapperList = arrayListOf<ImageMapperRecord>()
+                                recipe.image?.ident?.let { ident ->
+                                    mapperList.add(IMAGE_MAPPER.newRecord().apply {
+                                        val id = UUID.randomUUID().toString()
+                                        this.uuid = id
+                                        recipe.image?.uuid = id
+                                        this.oid = record.id
+                                        this.type = 0
+                                        this.imageUrl = ident
+                                    })
+                                }
+                                recipe.instruction?.filterNotNull()?.forEach {
+                                    it.image?.ident?.let { ident ->
+                                        mapperList.add(IMAGE_MAPPER.newRecord().apply {
+                                            val id = UUID.randomUUID().toString()
+                                            this.uuid = id
+                                            it.image?.uuid = id
+                                            this.oid = record.id
+                                            this.type = 0
+                                            this.imageUrl = ident
+                                        })
+                                    }
+                                    it.video?.url?.let { url ->
+                                        mapperList.add(IMAGE_MAPPER.newRecord().apply {
+                                            val id = UUID.randomUUID().toString()
+                                            this.uuid = id
+                                            it.video?.uuid = id
+                                            this.oid = record.id
+                                            this.type = 1
+                                            this.imageUrl = url
+                                        })
+                                    }
+                                }
+                                masterDSLContext.transaction { config ->
                                     if (mapperList.isNotEmpty())
                                         DSL.using(config).batchInsert(mapperList).execute()
                                     DSL.using(config).update(TB_RECIPE)
-                                        .set(TB_RECIPE.INGREDIENT, recipe.ingredient.toJson())
-                                        .set(TB_RECIPE.INSTRUCTION , recipe.instruction.toJson())
+                                        .set(TB_RECIPE.IMAGE, recipe.image.toJson())
+                                        .set(TB_RECIPE.INSTRUCTION, recipe.instruction.toJson())
                                         .set(TB_RECIPE.UPDATE_TIME, LocalDateTime.now())
                                         .where(TB_RECIPE.ID.eq(recipe.id)).execute()
                                 }
